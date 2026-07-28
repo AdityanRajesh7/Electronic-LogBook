ba57a0871c044b604121670b5080eeedb295ed60
x3O8 <abhijithkrishna200@gmail.com>
2026-07-28 12:52:07 +0530

    texture fixed

--- artifacts/mockup-sandbox/src/main.tsx
+++ artifacts/mockup-sandbox/src/main.tsx
@@ -3,6 +3,9 @@ import "@fontsource/open-sauce-one/400.css";
 import "@fontsource/open-sauce-one/500.css";
 import "@fontsource/open-sauce-one/600.css";
 import "@fontsource/open-sauce-one/700.css";
+import "@fontsource/roboto/latin-500.css";
+import "@fontsource/roboto/latin-600.css";
+import "@fontsource/roboto/latin-700.css";
 import App from "./App";
 import "./index.css";
 
